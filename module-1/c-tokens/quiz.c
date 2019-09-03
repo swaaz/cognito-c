1026265911519f5d701570bd20ef1b771a9cc5d8
swaaz <swaasthik.shetty07@gmail.com>
2019-09-03 22:39:08 +0530

    q1 added

--- module-1/c-tokens/quiz.c
+++ module-1/c-tokens/quiz.c
@@ -1,4 +1,5 @@
-struct quiz q1={4,}
+struct quiz q1={4,2,2,4,}
+
 int quiz1() //c-tokens
 {
     int x;
@@ -19,7 +20,57 @@ int quiz1() //c-tokens
             printf("Wrong\n");
             goto 1;
         }
-    2:
-        
-
+    2:  printf("2. The format identifier ‘%i’ is also used for _____ data type.\n1) char\n2) int\n3) float\n4) double"\n);
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q1.2)
+        {
+            printf("Correct\n");
+            goto 3;
+        }
+        else
+        {
+            printtf("wrong\n");
+            goto 2;
+        }
+    3:  printf("3. Which data type is most suitable for storing a number 65000 in a 32-bit system?\n1) signed short\n2) unsigned short\n3) long\n4) int");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q1.3)
+        {
+            printf("Correct\n");
+            goto 4;
+        }
+        else
+        {
+            printtf("wrong\n");
+            goto 3;
+        }
+    4:  printf("Which of the following is a User-defined data type?\n1) typedef int Boolean;\n2) typedef enum {Mon, Tue, Wed, Thu, Fri} Workdays;\n3) struct {char name[10], int age};\n4) all of the mentioned");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q1.4)
+        {
+            printf("Correct\n");
+            goto 5;
+        }
+        else
+        {
+            printtf("wrong\n");
+            goto 4;
+        }
+    5:  printf("What is the size of an int data type?\n1) 4 Bytes\n2) 8 Bytes\n3) Depends on the system/compiler\n4) Cannot be determined");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q1.5)
+        {
+            printf("Correct\n");
+            goto 6;
+        }
+        else
+        {
+            printtf("wrong\n");
+            goto 5;
+        }
+    6:  
 }
