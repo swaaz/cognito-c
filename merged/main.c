9ae9ac5c11de93fbd331b9d2b721eeaf77ea1f7f
swaaz <swaasthik.shetty07@gmail.com>
2019-09-08 01:06:43 +0530

    quiz 1 added

--- merged/main.c
+++ merged/main.c
@@ -14,6 +14,11 @@
 //#include<learn2.h>
 //#include<learn3.h>
 
+typedef struct
+{
+    int q[10];
+}quiz;
+
 int module_1()
 {system("clear");
     int o;
@@ -268,6 +273,95 @@ int learn4() //operators
     printf("  | >         | greater than   |  !a    | if a is true(1) the NOT a gives false(0) if a is false the NOT a is true                                    |\n");
     printf("  |___________|________________|________|_____________________________________________________________________________________________________________|\n");
     return 0;
+}
+struct quiz q1={4,2,2,4,3,3};
+
+int quiz1() //c-tokens
+{
+    int x;
+
+   1:   printf("\n1.Which of the following is not a valid declaration in C? \n");
+        printf("a.short int x;\nb.short int x;\nc.short x;\nd.unsigned short x;\n");
+        printf("Options\n");
+        printf("1.c & d\n2.b\n3.a\n4.all are valid\n");
+        printtf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q1.1) 
+        {
+            printf("Correct\n");
+            goto 2;
+        }
+        else 
+        {
+            printf("Wrong\n");
+            goto 1;
+        }
+    2:  printf("2. The format identifier ‘%i’ is also used for _____ data type.\n1) char\n2) int\n3) float\n4) double"\n);
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q1.2)
+        {
+            printf("Correct\n");
+            goto 3;
+        }
+        else
+        {
+            printtf("wrong\n");
+            goto 2;
+        }
+    3:  printf("3. Which data type is most suitable for storing a number 65000 in a 32-bit system?\n1) signed short\n2) unsigned short\n3) long\n4) int");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q1.3)
+        {
+            printf("Correct\n");
+            goto 4;
+        }
+        else
+        {
+            printtf("wrong\n");
+            goto 3;
+        }
+    4:  printf("Which of the following is a User-defined data type?\n1) typedef int Boolean;\n2) typedef enum {Mon, Tue, Wed, Thu, Fri} Workdays;\n3) struct {char name[10], int age};\n4) all of the mentioned");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q1.4)
+        {
+            printf("Correct\n");
+            goto 5;
+        }
+        else
+        {
+            printtf("wrong\n");
+            goto 4;
+        }
+    5:  printf("What is the size of an int data type?\n1) 4 Bytes\n2) 8 Bytes\n3) Depends on the system/compiler\n4) Cannot be determined");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q1.5)
+        {
+            printf("Correct\n");
+            goto 6;
+        }
+        else
+        {
+            printtf("wrong\n");
+            goto 5;
+        }
+    6:  printf("8. What is short int in C programming?\n 1) The basic data type of C\n2) Qualifier\n3) Short is the qualifier and int is the basic data type\n4) All of the mentioned");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q1.6);
+        {
+            printf("Correct\n");
+            goto 7;
+        }
+        else
+        {
+            printtf("wrong\n");
+            goto 6;
+        }
+    7:     
 }
 
 void main()
