841d53af2c6bd9259c494f5f50041db763483348
swaaz <swaasthik.shetty07@gmail.com>
2019-09-15 22:24:38 +0530

    q2 debugged

--- merged/main.c
+++ merged/main.c
@@ -394,6 +394,7 @@ int learn5() //strings
 }
 
 quiz q1={4,2,2,4,3,3,4,2,4,1}; //c-tokens
+quiz q2={4,4,4,3,2,3,4,4,3,4}; //variables
 
 int quiz1() //c-tokens
 {
@@ -533,6 +534,141 @@ int quiz1() //c-tokens
             printf("wrong\n");
             goto l1_10;
         }
+}
+int quiz2() // variables
+{
+     int x;
+     l2_1:   printf("\n1. A C variable cannot start with ? \n1. An alphabet\n2.. A number\n3. A special symbol other that underscore\n4. Both B and C\n");
+         printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q2.q[0])
+        {
+            printf("Correct\n");
+            goto l2_2;
+        }
+        else
+        {
+            printf("Wrong\n");
+            goto l2_1;
+        }
+    l2_2:  printf("2. A name having a few letters, numbers and special character _(underscore) is called\n1. keywords\n2. reserved keyword\n3. tokens\n4. identifiers\n");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q2.q[1])
+        {
+            printf("Correct\n");
+            goto l2_3;
+        }
+        else
+        {
+            printf("wrong\n");
+            goto l2_2;
+        }
+    l2_3:  printf("3. What is the scope of an external variable?\n1. Whole source file in which it is defined\n2. From the point of declaration to the end of the file in which it is defined\n3. Any source file in a program\n4. From the point of declaration to the end of the file being compiled\n");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q2.q[2])
+        {
+            printf("Correct\n");
+            goto l2_4;
+        }
+        else
+        {
+            printf("wrong\n");
+            goto l2_3;
+        }
+    l2_4:  printf("4. Variable names beginning with underscore is not encouraged. Why? \n1. It is not standardized\n2. To avoid conflicts since assemblers and loaders use such names\n3. To avoid conflicts since library routines use such names\n4. To avoid conflicts with environment variables of an operating system\n");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q2.q[3])
+        {
+            printf("Correct\n");
+            goto l2_5;
+        }
+        else
+        {
+            printf("wrong\n");
+            goto l2_4;
+        }
+    l2_5:  printf("5. In an assignment statement a=b; which of the following statement is true ?\n1. The variable a and the variable b are same.\n2. The value of b is assigned to variable a but if b changes later, it will not effect the value of variable a.\n3. The value of b is assigned to variable a but if b changes later, it will effect the value of variable\n4. The value of variable a is assigned to variable b, and the value of variable b is asssigned to variable a.\n");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q2.q[4])
+        {
+            printf("Correct\n");
+            goto l2_6;
+        }
+        else
+        {
+            printf("wrong\n");
+            goto l2_5;
+        }
+    l2_6:  printf("6. Which is an incorrect variable name?\n1. Id_No\n2. ID_NO\n3. IdNo\n4. Id No\n");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q2.q[5])
+        {
+            printf("Correct\n");
+            goto l2_7;
+        }
+        else
+        {
+            printf("wrong\n");
+            goto l2_6;
+        }
+    l2_7: printf("7.Which of the following is not valid variable names in 'C'?\n1. float_int, keyword, A1\n2. ANSI, ascii, cpu\n3. valid, variable, name\n4 None of these\n");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q2.q[6])
+        {
+            printf("Correct\n");
+            goto l2_8;
+        }
+        else
+        {
+            printf("wrong\n");
+            goto l2_7;
+        }
+    l2_8: printf("8. Which of the following cannot be used as identifiers?\n1. Letters\n2. Digits\n3. Underscores\n4. Spaces\n");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q2.q[7] )
+        {
+            printf("Correct\n");
+            goto l2_9;
+        }
+        else
+        {
+            printf("wrong\n");
+            goto l2_8;
+       }
+    l2_9:  printf("9. Variable names beginning with underscore is not encouraged. Why?\n1. It is not standard form\n2. To avoid conflicts since assemblers and loaders use such names\n3. To avoid conflicts since library routines use such names\n4. To avoid conflicts with environment variables of an operating system\n");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q2.q[8])
+        {
+            printf("Correct\n");
+            goto l2_10;
+        }
+        else
+        {
+            printf("wrong\n");
+            goto l2_9;
+        }
+    l2_10: printf("10 Which of the following is not a valid variable name declaration?\n1. float PI = 3.14;\n2. double PI = 3.14;\n3. int PI = 3.14;\n4. #define PI 3.14\n");
+        printf("Enter the choice : ");
+        scanf("%d",&x);
+        if(x==q2.q[9])
+        {
+            printf("Correct\n");
+            variables();
+        }
+        else
+        {
+            printf("wrong\n");
+            goto l2_10;
+        }
+
 }
 
 
