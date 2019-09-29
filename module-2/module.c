461039f04411fb98e3c4035f3b038bb8f1c184a5
swaaz <swaasthik.shetty07@gmail.com>
2019-09-29 20:49:21 +0530

    m2 added

---
+++ module-2/module.c
@@ -0,0 +1,24 @@
+int module_2()
+{
+    int o;
+    while(1)
+    {
+        printf("\n----------MODULE-2----------\n");
+        printf("1.\n4.BACK TO MAIN MENU\n5.EXIT\n");
+        printf("Enter the option : ");
+        scanf("%d",&o);
+        switch (o)
+        {
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4:return 0;
+            case 5:exit(0);
+
+
+            default:printf("Sorry!! It's an invalid option\n");
+
+        }
+    }
+    return 0;
+}
