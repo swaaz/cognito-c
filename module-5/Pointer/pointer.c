46b94a1697fbcf20766a3404c779a5dc4ffcde24
Sumanth@//20 <54536286+sumanthks20@users.noreply.github.com>
2020-01-18 16:18:50 +0530

    Update pointer.c

--- module-5/Pointer/pointer.c
+++ module-5/Pointer/pointer.c
@@ -1,7 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
 
-int main()
+int pointer()
 {
     printf("\n ********** pointer **********\n\n");
     printf(" Pointer Definition: Pointer is a variable that holds the address of another variable.\n");
