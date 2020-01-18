edc8ff824fb87a1b274eec16ac7af9759fb536e2
swaaz <swaasthik.shetty07@gmail.com>
2020-01-18 17:04:52 +0530

    updated

--- module-5/module_5.c
+++ module-5/module_5.c
@@ -1,17 +1,17 @@
 
-int module_1()
+int module_5()
 {
     int o;
     while(1)
     {
-        printf("\n----------MODULE-1----------\n");
-        printf("1.C TOKENS\n2.VARIABLES\n3.CONSTANTS\n4.BACK TO MAIN MENU\n5.EXIT\n");
+        printf("\n----------MODULE-5----------\n");
+        printf("1. POINTERS\n2. STRUCTURE\n3. PREPROCESSOR DIRECTIVES\n4.BACK TO MAIN MENU\n5.EXIT\n");
         printf("Enter the option : ");
         scanf("%d",&o);
         switch (o)
         {
-            case 1:structure(); break;
-            case 2:pointer(); break;
+            case 1:pointer(); break;
+            case 2:structure(); break;
             case 3:preprocessor_directives(); break;
             case 4:return 0;
             case 5:exit(0);
