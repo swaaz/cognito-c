0322acdad7fb1566fd62130859c2b2a329ec1e97
Sumanth@//20 <54536286+sumanthks20@users.noreply.github.com>
2020-03-09 21:32:51 +0530

    updated

--- module-3/Array and String/quiz.c
+++ module-3/Array and String/quiz.c
@@ -4,7 +4,7 @@ int quiz7()//arrays
 
    int x;
 
-    l7.1:   printf("\n1. What does the following function check for? (all necessary headers to be included and function is called from main) \n");
+    l7_1:   printf("\n1. What does the following function check for? (all necessary headers to be included and function is called from main) \n");
             printf("#define MAX 10\ntypedef struct stack\n{\n        int top;\n	int item[MAX];\n   }stack;\n   int function(stack *s)\n   {\n   if(s->top == -1)\n	    return 1;\n	 else return 0;\n   }\n");
             printf("1. full stack\n2. invalid index\n 3.  empty stack\n 4.infinite stack\n");
          printf("Enter the choice : ");
@@ -19,7 +19,7 @@ int quiz7()//arrays
             printf("Wrong\n");
             goto l7.1;
         }
-    l7.2:  printf("\n2. Arrays are best data structures");
+    l7_2:  printf("\n2. Arrays are best data structures");
         printf("1. for relatively permanent collections of data\n2. for the size of the structure and the data in the structure are constantly changing\n 3.   for both of above situation\n 4.for non of above situation\n");
         printf("Enter the choice : ");
         scanf("%d",&x);
@@ -33,7 +33,7 @@ int quiz7()//arrays
             printf("wrong\n");
             goto l7.2;
         }
-    l7.3:  printf("\n3. A …………………… does not keep track of address of every element in the list.")
+    l7_3:  printf("\n3. A …………………… does not keep track of address of every element in the list.")
         printf("1. Stack\n2. String\n 3. Linear array\n 4.Queue\n");
         printf("Enter the choice : ");
         scanf("%d",&x);
@@ -47,7 +47,7 @@ int quiz7()//arrays
             printf("wrong\n");
             goto l7.3;
         }
-    l7.4:  printf("4. Each array declaration need not give, implicitly or explicitly, the information about");
+    l7_4:  printf("4. Each array declaration need not give, implicitly or explicitly, the information about");
         printf("1. the name of array\n2. the data type of array\n 3. the first data from the set to be stored\n 4.the index set of the array\n");
         printf("Enter the choice : ");
         scanf("%d",&x);
@@ -61,7 +61,7 @@ int quiz7()//arrays
             printf("wrong\n");
             goto l7.4;
         }
-    l7.5:  printf("5. What does ‘stack underflow’ refer to?\n1. accessing item from an undefined stack\n2. adding items to a full stack\n3. removing items from an empty stack\n4. index out of bounds exception\n");
+    l7_5:  printf("5. What does ‘stack underflow’ refer to?\n1. accessing item from an undefined stack\n2. adding items to a full stack\n3. removing items from an empty stack\n4. index out of bounds exception\n");
         printf("Enter the choice : ");
         scanf("%d",&x);
         if(x==q7.5)
@@ -74,7 +74,7 @@ int quiz7()//arrays
             printf("wrong\n");
             goto l7.5;
         }
-    l7.6:  printf("6. Which of the following real world scenarios would you associate with a stack data structure?\n1. piling up of chairs one above the other\n2. people standing in a line to be serviced at a counter\n3. offer services based on the priority of the customer\n4. tatkal Ticket Booking in IRCTC\n");
+    l7_6:  printf("6. Which of the following real world scenarios would you associate with a stack data structure?\n1. piling up of chairs one above the other\n2. people standing in a line to be serviced at a counter\n3. offer services based on the priority of the customer\n4. tatkal Ticket Booking in IRCTC\n");
         printf("Enter the choice : ");
         scanf("%d",&x);
         if(x==q7.6)
@@ -87,7 +87,7 @@ int quiz7()//arrays
             printf("wrong\n");
             goto l7.6;
         }
-    l7.7: printf("7. What is the time complexity of pop() operation when the stack is implemented using an array?\n1. O(1)\n2. O(n)\n3. (logn)\n4. O(nlogn)\n");
+    l7_7: printf("7. What is the time complexity of pop() operation when the stack is implemented using an array?\n1. O(1)\n2. O(n)\n3. (logn)\n4. O(nlogn)\n");
         printf("Enter the choice : ");
         scanf("%d",&x);
         if(x==q7.7)
@@ -100,7 +100,7 @@ int quiz7()//arrays
             printf("wrong\n");
             goto l7.7;
         }
-    l7.8: printf("8. Which of the following array position will be occupied by a new element being pushed for a stack of size N elements(capacity of stack > N).\n1. S[N-1]\n2.  S[N]\n3. S[1]\n4. S[0]\n");
+    l7_8: printf("8. Which of the following array position will be occupied by a new element being pushed for a stack of size N elements(capacity of stack > N).\n1. S[N-1]\n2.  S[N]\n3. S[1]\n4. S[0]\n");
         printf("Enter the choice : ");
         scanf("%d",&x);
         if(x==q6.8)
@@ -113,7 +113,7 @@ int quiz7()//arrays
             printf("wrong\n");
             goto l7.8;
        }
-    l7.9:  printf("9. Which of the following data structure can’t store the non-homogeneous data elements?\n1. Arrays\n2.	Records\n3. Pointers\n4. Stacks\n");
+    l7_9:  printf("9. Which of the following data structure can’t store the non-homogeneous data elements?\n1. Arrays\n2.	Records\n3. Pointers\n4. Stacks\n");
         printf("Enter the choice : ");
         scanf("%d",&x);
         if(x==q7.9)
@@ -126,7 +126,7 @@ int quiz7()//arrays
             printf("wrong\n");
             goto l7.9;
         }
-    l7.10: printf("10. The memory address of the first element of an array is called\n1. floor address\n2. foundation address\n3. first address\n4. base address\n");
+    l7_10: printf("10. The memory address of the first element of an array is called\n1. floor address\n2. foundation address\n3. first address\n4. base address\n");
         printf("Enter the choice : ");
         scanf("%d",&x);
         if(x==q7.10)
